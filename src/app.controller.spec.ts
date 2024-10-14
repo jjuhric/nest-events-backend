5d6551cb2d6909aca6e0bac80bd74df4ed88cfd4
Jeff Uhrick <jjuhric@gmail.com>
2024-10-14 00:12:11 -0500

    correcting test and removing use of adminer

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello Jeffery\'s World!"', () => {
       expect(appController.getHello()).toBe('Hello Jeffery\'s World!');
     });
   });
