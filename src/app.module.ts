b18d73b9321403b797e4582cbf39e8eaa5200ec1
Jeff Uhrick <jjuhric@gmail.com>
2024-10-14 21:41:43 -0500

    adding typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'nest_events',
+    username: 'root',
+    password: 'example',
+    entities: [Event],
+    synchronize: true // ONLY use this for development
+  })],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
